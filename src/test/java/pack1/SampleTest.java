b2531ef49c918e737eea0093911034dbc395f53e
manju1702 <manjusahu1702@gmail.com>
2023-11-30 12:52:00 +0530

    "sample test"

---
+++ src/test/java/pack1/SampleTest.java
@@ -0,0 +1,16 @@
+package pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+	@Test
+	public void m1Test()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+	}
+
+}
